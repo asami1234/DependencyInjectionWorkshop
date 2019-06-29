9cfbe80f167dff927b5bea79e274e83d36bffe24
Kevin Yang <asami1234@hotmail.com>
2019-06-29 10:49:10 +0800

    fast done

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -13,6 +13,18 @@ namespace DependencyInjectionWorkshop.Models
     {
         public bool Verify(string accountId, string password, string otp)
         {
+            var httpClient = new HttpClient() {BaseAddress = new Uri("http://joey.com/")};
+            
+            // 檢查帳號是否被鎖定
+            var isLockedResponse = httpClient.PostAsJsonAsync("api/failedCounter/IsLocked", accountId).Result;
+            
+            isLockedResponse.EnsureSuccessStatusCode();
+            if (isLockedResponse.Content.ReadAsAsync<bool>().Result)
+            {
+                throw new FailedTooManyTimesException();
+            }
+            
+            // 驗證密碼
             string currentPassword;
             using (var connection = new SqlConnection("datasource=db,password=abc"))
             {
@@ -29,8 +41,8 @@ namespace DependencyInjectionWorkshop.Models
             }
 
             var hashPassword = hash.ToString();
-
-            var httpClient = new HttpClient() {BaseAddress = new Uri("http://joey.com/")};
+            
+            // 驗證 OTP
             var response = httpClient.PostAsJsonAsync("api/otps", accountId).Result;
             string currentOtp;
             if (response.IsSuccessStatusCode)
@@ -42,16 +54,41 @@ namespace DependencyInjectionWorkshop.Models
                 throw new Exception($"web api error, accountId:{accountId}");
             }
 
+            // 驗證登入
             if (hashPassword == currentPassword && otp == currentOtp)
             {
+                //驗證成功
+                var resetResponse = httpClient.PostAsJsonAsync("api/failedCounter/Reset", accountId).Result;
+                resetResponse.EnsureSuccessStatusCode();
+
                 return true;
             }
             else
             {
+                // 錯誤發送 Slack 通知
                 var slackClient = new SlackClient("my api token"); 
                 slackClient.PostMessage(postMessageResponse => { }, "my channel", "my message", "my bot name");
+                
+                // 發送錯誤次數
+                var addFailedCountResponse = httpClient.PostAsJsonAsync("api/failedCounter/Add", accountId).Result;
+                addFailedCountResponse.EnsureSuccessStatusCode();
+
+                // 錯誤次數寫入 LOG
+                var failedCountResponse =
+                    httpClient.PostAsJsonAsync("api/failedCounter/GetFailedCount", accountId).Result;
+
+                failedCountResponse.EnsureSuccessStatusCode();
+
+                var failedCount = failedCountResponse.Content.ReadAsAsync<int>().Result;
+                var logger = NLog.LogManager.GetCurrentClassLogger();
+                logger.Info($"accountId:{accountId} failed times:{failedCount}");
+                
                 return false;
             }
         }
+    }
+
+    public class FailedTooManyTimesException : Exception
+    {
     }
 }
